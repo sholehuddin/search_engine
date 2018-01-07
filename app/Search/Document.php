a7cd4b9d2371405a0f6046763860188f545b1ee0
Ripas Filqadar <ripas26adm@gmail.com>
2018-01-07 12:44:53 +0700

    create base class for search

--- app/Search/Document.php
+++ app/Search/Document.php
@@ -2,39 +2,13 @@
 
 namespace App\Search;
 
-class Document
+class Document extends Base
 {
-	public $keywords, $page, $per_page, $results;
-	public function __construct(array $params)
-	{
-		if (isset($params["keywords"])){
-	    	$this->keywords = $params["keywords"];
-		}else{
-	    	$this->keywords = '';	
-		}
-		if (isset($params["page"])){
-	    	$this->page = $params["page"];
-		}else{
-	    	$this->page = 1;
-		}
-
-		if (isset($params["per_page"])) {
-	    	$this->per_page = $params["per_page"];
-		}else{
-	    	$this->per_page = 10;
-		}
-	}
-
 	public function results()
 	{
 		if (is_null($this->results)){
 			$this->results = \App\Document::search($this->keywords)->paginate($this->per_page);
 		}
 		return $this->results;
-	}
-
-	public function data()
-	{
-		return $this->results->toArray()["data"];
 	}
 }
